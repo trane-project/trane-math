b8c854e0746d9e59d04cf5b98ea0d9d3c8819f47
Martin Martinez Rivera <mrtnz.rvr@gmail.com>
2023-03-04 17:00:00 -0600

    Fix test and upgrade trane

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,20 @@ fn main() {}
 #[cfg(test)]
 mod test {
     use anyhow::Result;
-    use std::{fs, path::Path};
     use trane::{course_library::CourseLibrary, Trane};
 
     #[test]
     fn verify_courses() -> Result<()> {
-        let courses_path = Path::new("courses");
-        let trane = Trane::new(&std::env::current_dir()?, Path::new("courses"))?;
+        // Copy courses directory to a temp directory.
+        let temp_dir = tempfile::tempdir()?;
+        fs_extra::dir::copy(
+            "courses",
+            temp_dir.path(),
+            &fs_extra::dir::CopyOptions::new(),
+        )?;
+
+        let trane = Trane::new(&temp_dir.path(), &temp_dir.path())?;
         assert!(trane.get_all_exercise_ids()?.len() > 0);
-        fs::remove_dir_all(courses_path.join(".trane"))?;
         Ok(())
     }
 }
